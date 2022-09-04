3f3f4060fe2da1b8ffd0eac40c0525286bfe15ca
ChaeHunPark <cogns8571@naver.com>
2022-09-04 21:56:42 +0900

    이모션 값 받아서 컨텐츠 뿌리기

--- client/src/components/MoreBannerPage/MoreBannerPage.js
+++ client/src/components/MoreBannerPage/MoreBannerPage.js
@@ -1,7 +1,7 @@
 import { withRouter } from 'react-router-dom';
 import Header from '../Common/Header/Header';
 import MoreContents from './Section/MoreContents';
-
+import Footer from '../Common/Footer/Footer';
 
 
 
@@ -10,6 +10,7 @@ function MoreBannerPage(){
     return(<>
     <Header/>
     <MoreContents/>
+    <Footer/>
     </>    
     )
 }
--- server/routes/contents.js
+++ server/routes/contents.js
@@ -23,6 +23,62 @@ router.post('/contents/getContentsP', (req, res) => {
         })
 })
 
+router.post('/contents/emotion/getFear',(req, res) =>{
+    Contents.find({emotion:'공포'})
+    .exec((err, fear) =>{
+        if (err) return res.status(400).send(err);
+        return res.status(200).json({success: true, fear});
+    })
+})
+
+router.post('/contents/emotion/getSurprised',(req, res) =>{
+    Contents.find({emotion:'놀람'})
+    .exec((err, surprised) =>{
+        if (err) return res.status(400).send(err);
+        return res.status(200).json({success: true, surprised});
+    })
+})
+
+router.post('/contents/emotion/getAngry',(req, res) =>{
+    Contents.find({emotion:'분노'})
+    .exec((err, angry) =>{
+        if (err) return res.status(400).send(err);
+        return res.status(200).json({success: true, angry});
+    })
+})
+
+router.post('/contents/emotion/getSad',(req, res) =>{
+    Contents.find({emotion:'슬픔'})
+    .exec((err, sad) =>{
+        if (err) return res.status(400).send(err);
+        return res.status(200).json({success: true, sad});
+    })
+})
+
+router.post('/contents/emotion/getNeutral',(req, res) =>{
+    Contents.find({emotion:'중립'})
+    .exec((err, neutral) =>{
+        if (err) return res.status(400).send(err);
+        return res.status(200).json({success: true, neutral});
+    })
+})
+
+router.post('/contents/emotion/getHappy',(req, res) =>{
+    Contents.find({emotion:'행복'})
+    .exec((err, happy) =>{
+        if (err) return res.status(400).send(err);
+        return res.status(200).json({success: true, happy});
+    })
+})
+
+router.post('/contents/emotion/getHate',(req, res) =>{
+    Contents.find({emotion:'혐오'})
+    .exec((err, hate) =>{
+        if (err) return res.status(400).send(err);
+        return res.status(200).json({success: true, hate});
+    })
+})
+
 router.post('/login/:id', (req, res) => {
     Contents.findOne({ _id: req.body.movieDbId }, (err, contents) => {
         if (contents) {
@@ -51,5 +107,7 @@ router.post('/contents/moreContents', (req, res) => {
     })
 
 })
+
+
 
 module.exports = router;
